cd4fa9d5f5a3e21ea92b503b35aa73276d8f5998
DE GUIA, MARK DANIEL C. <markdanieldeguia0@gmail.com>
2025-02-18 21:58:22 +0800

    Add files via upload

---
+++ CS1A_DE GUIA_CS1A/CS1A_MDdeguia_C1-A1.js
@@ -0,0 +1,35 @@
+// First student
+let first_name = "Mark daniel ";
+let middle_name = "Castillo ";
+let lastname = "De Guia";
+let birth_date = "October 2, 2006";
+let birth_place = "San Jose, Candon City, Ilocos Sur, Philippines 2710";
+let address = "Bagani Campo, Candon City, Ilocos Sur, Philippines 2710";
+let course_and_year = "Bachelor of Science in Computer Science 1A";
+let dream_job = "Software Engineer";
+
+// Second student
+let first_name2 = "Mark ";
+let middle_name2 = "Anaya ";
+let lastname2 = "Habungan";
+let birth_date2 = "November 3, 2006";
+let birth_place2 = "Sanjuan, Santa lucia, Ilocos Sur, Philippines 2712";
+let address2 = "Sanjuan, Santa lucia, Ilocos Sur, Philippines 2712";
+let course_and_year2 = "Bachelor of Science in Computer Science 1A";
+let dream_job2 = "Software Engineer";
+
+// First student
+let first_student = first_name.toUpperCase() + " " + middle_name.toUpperCase() + " " + lastname.toUpperCase() +
+" was born " + birth_date + " at " + birth_place.toUpperCase() + " and currently living at " + address.toUpperCase() + " " + first_name.toLowerCase() + middle_name.toLowerCase() + lastname.toLowerCase() + " is taking up " + course_and_year.toLowerCase() + " and dreams to be " + dream_job + " after graduation."
+
+// Second Student
+let second_student = first_name2.toUpperCase() + " " + middle_name2.toUpperCase() + " " + lastname2.toUpperCase() +
+" was born " + birth_date2 + " at " + birth_place2.toUpperCase() + " and currently living at " + address2.toUpperCase() + " " + first_name2.toLowerCase() + middle_name2.toLowerCase() + lastname2.toLowerCase() + " is taking up " + course_and_year2.toLowerCase() + " and dreams to be " + dream_job2 + " after graduation."
+
+
+
+// First student print
+console.log(first_student);
+
+// Second student print
+console.log(second_student);
